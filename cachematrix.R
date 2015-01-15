211eb062b2c312f300c551c768d50336fbe004c5
unknown <preeteshs2012@gmail.com>
2015-01-15 10:38:49 +0530

    submission

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,7 @@
 ##If exact replica of the matrix exists from the list, the function returns the inverse from the cached list.
 ##else it calculates the inverse and stores in the list.
 
+
 ## The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
 ##set the value of the vector
 ##get the value of the vector
@@ -24,7 +25,7 @@ list(set=set,get=get,
 
 ## The below function computes the inverse of the special "vector" created with the above function.
 ##However, it first checks to see if the inverse has already been computed before.
-##If so, it gets the inverse from the cache and skips the calculation.
+##If so, it gets the inverse from the cache and skips the calculation thus avoiding waste of computing time.
 ##Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
